6879b1898c13826f687c6d03f600643dc2567520
MansiRavindraMohan <manasimohan29@gmail.com>
2023-04-30 20:48:52 +0530

    Add: Example of types of constructors

---
+++ Day3B_PracticeProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Day3B_PracticeProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            TypesConstructors typesConstructors = new TypesConstructors();
+            TypesConstructors types=new TypesConstructors(145,"Mansi");
+            TypesConstructors typesTwo = new TypesConstructors(types);
+        }
+    }
+}
---
+++ Day3B_PracticeProblem/TypesConstructors.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Day3B_PracticeProblem
+{
+    public class TypesConstructors
+    {
+        //Default Constructor
+        public TypesConstructors() 
+        {
+            Console.WriteLine("This is example of default constructor");
+        }
+        //Parameterised Constructor
+        public int Id;
+        public string Name;
+        public TypesConstructors(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+            Console.WriteLine("This is example of Parameterised constructor");
+            Console.WriteLine(name);
+            Console.WriteLine(id);
+        }
+        //Copy Constructor
+        public TypesConstructors(TypesConstructors other)
+        {
+            this.Id = other.Id;
+            this.Name = other.Name;
+            Console.WriteLine("This is example of Copy constructor");
+            Console.WriteLine(other.Name);
+            Console.WriteLine(other.Id);
+        }
+    }
+}
